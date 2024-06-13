1c60b019d9583ac200fd2c66b5205eab95552659
Shahrukh Khan <61946634+shahrukh263@users.noreply.github.com>
2024-06-13 20:57:49 +0530

    project added

---
+++ src/App.js
@@ -0,0 +1,11 @@
+
+import './App.scss';
+function App(){
+  return(
+    <div className='main-container'>
+      <h1>Hello</h1>
+      <h1 className="title">Hello</h1>
+    </div>
+  )
+}
+export default App;
